7b7f6d5c6f110fb734117b74d269be2ff92e40f1
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-04-08 14:52:33 +0200

    Add mosca server. Remove mongoose dependency.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('pillars');
-const mongoose = require('mongoose');
+const mosca = require('mosca');
 
 // Load .env config.
 console.info('Cargando archivos de configuración');
@@ -9,23 +9,42 @@ require('dotenv').config();
 console.info('Añadiendo rutas.');
 require('./src/routes');
 
-// Configure pillars project and connect to mongoDB
-app.config.debug = process.env.DEBUG_MODE;
-// app.config.favicon = "favico.ico";
+const formula_rt = {
+	type: 'mongo',
+	url: process.env.MONGODB_URI,
+	pubsubCollection: 'formula_rt',
+	mongo: {}
+};
 
-console.info(`Conectan con la base de datos MongoDB en ${ process.env.MONGODB_URI }`);
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true }, err => {
-	if (err) {
-		throw new Error(`Error conectandose a la base de datos: ${err}`);
-	}
+const settings = {
+	port: parseInt(process.env.MQTT_PORT),
+	host: process.env.HOST,
+	backend: formula_rt
+};
 
-	console.info('Conexión a base de datos realizada con exito. Iniciando servidor...');
+const server = new mosca.Server(settings);
+
+server.on('clientConnected', client => {
+	console.log('client connected', client.id);
+	console.log("-------- FUCKING CONNECTED!!! ---")
+});
+
+// fired when a message is received
+server.on('published', (packet, client) => {
+	console.log('Published', packet.payload);
+	console.log("-------- FUCKING MSG!!! ---")
+});
+
+// fired when the mqtt server is ready
+server.on('ready', () => {
+	// Configure pillars project and connect to mongoDB
+	app.config.debug = process.env.DEBUG_MODE;
+	// app.config.favicon = "favico.ico";
 
 	app.services.get('http').configure({
 		host: process.env.HOST,
-		port: process.env.PORT
+		port: parseInt(process.env.WEB_PORT)
 	}).start();
 
-	console.info(`Servidor corriendo en: http://${ process.env.HOST }:${ process.env.PORT }`);
+	console.info(`Servidor corriendo en: http://${ process.env.HOST }:${ process.env.WEB_PORT }`);
 });
-
